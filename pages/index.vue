56ba964f1a04d158f64b938b6968cc3c99c1a132
Dennis Dipsaus <dennis.spierenburg@hotmail.com>
2019-02-11 16:05:22 +0100

    at least i have something now

--- pages/index.vue
+++ pages/index.vue
@@ -19,15 +19,14 @@
 		</section>
 		<transition name="fade">
 			<section
-				v-if="state !== 'artist'"
 				id="data"
 				class="start active"
 			>
 				<div class="artists__list">
 					<ul>
-						<li v-for="(artist, index) in topArtists" :key="index">
+						<li v-for="(artist, index) in topArtists" :key="index" @click="artistToCompare(index)">
 							<span
-								:class="{ 'hover': index === hoverElement }"
+								:class="{ 'hover': index === hoverElement, 'compared' : compareArtists.includes(index) }"
 								:id="index"
 								class="artists__link"
 							>
@@ -39,7 +38,22 @@
 						</li>
 					</ul>
 				</div>
-				<div class="svg_container">
+				<transition name="fade">
+					<div
+						v-if="state !== 'artist'"
+						class="svg_container"
+					>
+					</div>
+				</transition>
+				<transition name="fade">
+					<div
+						v-if="state === 'artist'"
+						class="svg_container--compare"
+					>
+					</div>
+				</transition>
+				<div :class="{ active: compareArtists.length > 1 && state === 'home' }" class="compare">
+					<button class="compare--button" @click="compareArtistsInit(true)">Compare selected artists</button>
 				</div>
 			</section>
 		</transition>
@@ -65,8 +79,14 @@ export default {
 			topArtists: {},
 			artistData: [],
 			state: 'home',
-			artist: ''
+			artists: [],
+			compareArtists: []
 		};
+	},
+	watch: {
+		compareArtists: {
+			handler: 'compareArtistsInit'
+		}
 	},
 	methods: {
 		async getData() {
@@ -79,7 +99,7 @@ export default {
 					api_key: apiKey,
 					method: 'user.getTopTracks',
 					format: 'json',
-					period: '1month',
+					period: '7day',
 					user: this.userName
 				}
 			});
@@ -128,15 +148,11 @@ export default {
 				return accumulator;
 			}, []);
 			this.topArtists.sort((a, b) => this.getTimesPlayed(b.children) - this.getTimesPlayed(a.children));
-			// this.topArtists.splice(10, this.topArtists.length - 1);
 		},
 		createChart() {
 			// source: https://beta.observablehq.com/@mbostock/d3-sunburst
 			this.collectAllArtists();
 			let loaded = false;
-			setTimeout(() => {
-				loaded = true;
-			}, 500);
 			const reducer = (accumulator, currentvalue) => accumulator + Number(currentvalue.playcount);
 			this.totalTimesPlayed = this.topTracks.track.reduce(reducer, 0);
 			//d3 moments
@@ -149,7 +165,7 @@ export default {
 				children: this.topArtists
 			};
 
-			const color = d3.scaleOrdinal(d3.schemeBlues[8]);
+			const color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, newData.children.length + 1));
 			const root = d3.partition().size([2 * Math.PI, radius])(
 				d3
 					.hierarchy(newData)
@@ -185,23 +201,14 @@ export default {
 
 			const formatHover = (data) => {
 				if (data.playcount) {
-					return `${data.name} - played: ${data.playcount}`;
+					return `${data.name} - Played ${data.playcount} times`;
 				} else {
-					return data.name;
+					const amount = this.getTimesPlayed(data.children);
+					return `${data.name} - Played ${amount} times`;
 				}
 			};
-			const svg = element
-				.append('svg')
-				.attr('class', 'svg__element')
-				.attr('width', width)
-				.attr('height', height)
-				.attr('viewBox', `0 0 ${width} ${height}`)
-				.attr('perserveAspectRatio', 'xMinYMid')
-				.append('g')
-				.attr('class', 'svg__element-g')
-				.attr('transform', `translate(${width / 2}, ${height / 2})`);
 
-			const arc = d3
+			let arc = d3
 				.arc()
 				.startAngle((d) => d.x0)
 				.endAngle((d) => d.x1)
@@ -210,7 +217,7 @@ export default {
 				.innerRadius((d) => d.y0)
 				.outerRadius((d) => d.y1 - 1);
 
-			function arcTweenPath(a, i) {
+			function arcTweenPath(a) {
 				// (a.x0s ? a.x0s : 0) -- grab the prev saved x0 or set to 0 (for 1st time through)
 				// avoids the stash() and allows the sunburst to grow into being
 				var oi = d3.interpolate(
@@ -227,13 +234,25 @@ export default {
 				}
 				return tween;
 			}
+
+			const svg = element
+				.append('svg')
+				.attr('class', 'svg__element')
+				.attr('width', width)
+				.attr('height', height)
+				.attr('viewBox', `0 0 ${width} ${height}`)
+				.attr('perserveAspectRatio', 'xMinYMid')
+				.append('g')
+				.attr('class', 'svg__element-g')
+				.attr('transform', `translate(${width / 2}, ${height / 2})`);
+
 			svg
 				.append('g')
 				.attr('class', 'middle-text')
 				.append('text')
 				.attr('transform', 'translate(0, 0)')
 				.style('text-anchor', 'middle')
-				.text(`Total songs: ${this.totalTimesPlayed}`);
+				.text(`Total songs played: ${this.totalTimesPlayed}`);
 
 			svg
 				.append('g')
@@ -242,6 +261,7 @@ export default {
 				.data(root.descendants().filter((d) => d.depth))
 				.enter()
 				.append('path')
+				.attr('class', 'paths')
 				.on('mouseover', (d, i) => {
 					if (loaded) {
 						if (d.depth === 2) {
@@ -253,6 +273,14 @@ export default {
 						el.scrollIntoView();
 						this.hoverElement = i;
 					}
+				})
+				.on('click', (d, i) => {
+					if (d.depth === 2) {
+						let artistName = d.parent.data.name;
+						const index = newData.children.findIndex((artist) => artist.name === artistName);
+						i = index;
+					}
+					this.artistToCompare(i);
 				})
 				.on('mouseleave', () => {
 					this.hoverElement = -1;
@@ -269,11 +297,8 @@ export default {
 					return arcTweenPath(d, i);
 				});
 
-			svg
-				.data(root.descendants().filter((d) => d.depth))
-				.enter()
-				.append('title')
-				.text((d) => formatHover(d.data));
+			const allPaths = d3.selectAll('path');
+			allPaths.append('title').text((d) => formatHover(d.data));
 
 			svg
 				.append('g')
@@ -296,27 +321,44 @@ export default {
 				.style('opacity', 1)
 				.text((d) => formatName(d.data));
 
-			//
-			// window.addEventListener('resize', () => {
-			// 	height = window.innerHeight;
-			// 	width = window.innerWidth / 2;
-			// 	radius = Math.min(width, (height / 4) * 3) / 2;
-			//
-			// 	const update = d3.select('.svg__element');
-			// 	const updatePath = d3.select('.arc');
-			// 	const updateG = d3.select('.svg__element-g');
-			//
-			// 	updateSvg
-			// 		.attr('width', width)
-			// 		.attr('height', height)
-			// 		.attr('viewBox', `0 0 ${width} ${height}`)
-			// 		.attr('perserveAspectRatio', 'xMinYMid');
-			//
-			// 	updateG.attr('transform', `translate(${width / 2}, ${height / 2})`);
-			//
-			// 	updatePath.data(pie(data));
-			// });
+			window.addEventListener('resize', () => {
+				height = window.innerHeight;
+				width = (window.innerWidth / 4) * 3;
+				radius = Math.min(width, (height / 10) * 9) / 2;
+				const update = d3.select('.svg__element');
+				const updateG = d3.select('.svg__element-g');
+				const allPaths = d3.selectAll('.paths');
+
+				update
+					.attr('class', 'svg__element')
+					.attr('width', width)
+					.attr('height', height)
+					.attr('viewBox', `0 0 ${width} ${height}`)
+					.attr('perserveAspectRatio', 'xMinYMid');
+
+				updateG.attr('class', 'svg__element-g').attr('transform', `translate(${width / 2}, ${height / 2})`);
+
+				arc = d3
+					.arc()
+					.startAngle((d) => d.x0)
+					.endAngle((d) => d.x1)
+					.padAngle((d) => Math.min((d.x1 - d.x0) / 2, 0.005))
+					.padRadius(radius / 2)
+					.innerRadius((d) => d.y0)
+					.outerRadius((d) => d.y1 - 1);
+
+				allPaths
+					.transition()
+					.ease(d3.easeCubic)
+					.duration(transitionTime)
+					.attrTween('d', function(d, i) {
+						return arcTweenPath(d, i);
+					});
+			});
 			window.scrollTo(0, height);
+			setTimeout(() => {
+				loaded = true;
+			}, 500);
 		},
 		getArtistList(originalData, maxNr = this.maxArtistsPerPage) {
 			if (originalData && originalData.track && originalData.track.length) {
@@ -328,15 +370,153 @@ export default {
 				return [];
 			}
 		},
-		changeState(name) {
-			this.state = 'artist';
-			this.artist = name.split(' ').join('+');
-			this.getArtistInfo(this.artist);
+		async compareArtistsInit(clicked = false) {
+			if (clicked === true) {
+				this.state = 'artist';
+			}
+			if (this.compareArtists.length < 2 && this.state === 'artist') {
+				this.state = 'home';
+				setTimeout(() => {
+					this.createChart();
+				});
+			}
+			const newArtists = await Promise.all(
+				this.compareArtists.map(async (index) => {
+					const name = this.topArtists[index].name.split(' ').join('+');
+					return await this.getArtistInfo(name);
+				})
+			);
+			if (newArtists.length) {
+				this.artists = newArtists;
+				this.createSecondChart();
+			}
+		},
+		createSecondChart() {
+			const artists = this.artists;
+			const date = this.getLastWeek(0);
+			const lastWeek = this.getLastWeek(7);
+
+			const artistKeys = this.compareArtists.map((index) => {
+				return this.topArtists[index].name;
+			});
+
+			let names = [];
+			let namesCreated = false;
+			let dataCreated = artists.map((artist) => {
+				let newArtist = [];
+				// let totalVal = 0;
+				for (let i = lastWeek.getTime() / 1000; i < date.getTime() / 1000; i += 10800) {
+					let value = 0;
+					if (!namesCreated) {
+						names.push(i);
+					}
+					const contains = artist.filter((singleArtist) => {
+						if (singleArtist.date.uts > i && singleArtist.date.uts < i + 10800) {
+							return true;
+						}
+					});
+					value = contains.length;
+					newArtist.push(value);
+				}
+				namesCreated = true;
+				return newArtist;
+			});
+
+			let length = dataCreated[0].length;
+			let totals = [];
+			for (let i = 0; i < length; i++) {
+				let startValue = 0;
+				dataCreated.forEach((data, index) => {
+					const val = data[i];
+					data[i] = [startValue, startValue + data[i], artistKeys[index]];
+					startValue += val;
+				});
+				totals.push(startValue);
+			}
+			dataCreated.keys = artistKeys;
+			dataCreated.names = names;
+			dataCreated.totals = totals;
+
+			let height = window.innerHeight,
+				width = (window.innerWidth / 4) * 3;
+
+			const element = d3.select('.svg_container--compare');
+
+			const svg = element
+				.append('svg')
+				.attr('class', 'svg__element--compare')
+				.attr('width', width)
+				.attr('height', height)
+				.attr('viewBox', `0 0 ${width} ${height}`)
+				.attr('perserveAspectRatio', 'xMinYMid');
+			// .attr('class', 'svg__element-g')
+			// .attr('transform', `translate(${width / 2}, ${height / 2})`);
+
+			const margin = { top: 50, right: 40, bottom: 150, left: 40 };
+			const margin2 = { top: 430, right: 20, bottom: 30, left: 40 };
+			const data = dataCreated;
+
+			const g = svg
+				.append('g')
+				.attr('class', 'svg__element-g')
+				.attr('transform', `translate(${margin.left}, ${margin.top})`);
+			var x = d3
+				.scaleBand()
+				.rangeRound([0, width - margin.left - margin.right])
+				.padding(0.3)
+				.align(0.3);
+
+			var y = d3.scaleLinear().rangeRound([height - margin.top - margin.bottom, 0]);
+
+			const color = d3.scaleOrdinal().range(['#98abc5', '#8a89a6', '#7b6888', '#6b486b', '#a05d56', '#d0743c', '#ff8c00']);
+			x.domain(data.names);
+			y.domain([0, d3.max(data.totals)]).nice();
+
+			g.append('g')
+				.attr('class', 'axis axis--y')
+				.call(d3.axisLeft(y).ticks(10, 's'))
+				.append('text')
+				.attr('x', 2)
+				.attr('y', y(y.ticks(10).pop()))
+				.attr('dy', '0.35em')
+				.attr('text-anchor', 'start')
+				.attr('fill', '#000')
+				.text('Population');
+
+			g.append('g')
+				.attr('class', 'axis axis--x')
+				.attr('transform', `translate(0, ${height - margin.top - margin.bottom})`)
+				.call(d3.axisBottom(x));
+
+			g.selectAll('.serie')
+				.data(data)
+				.enter()
+				.append('g')
+				.attr('class', 'serie')
+				.attr('fill', (d, i) => {
+					return color(d[i][2]);
+				})
+				.selectAll('rect')
+				.data((d) => {
+					return d;
+				})
+				.enter()
+				.append('rect')
+				.attr('x', (d, i) => {
+					return x(data.names[i]);
+				})
+				.attr('y', (d) => {
+					return y(d[1]);
+				})
+				.attr('height', (d) => {
+					return y(d[0]) - y(d[1]);
+				})
+				.attr('width', x.bandwidth());
 		},
 		async getArtistInfo(artist) {
 			const apiKey = '9295242e7b99eba039f7147793f3bc23';
-			const date = this.getLastWeek(7);
-			const lastWeek = this.getLastWeek(12);
+			const date = this.getLastWeek(0);
+			const lastWeek = this.getLastWeek(7);
 			const response = await this.$axios({
 				method: 'get',
 				url: 'https://ws.audioscrobbler.com/2.0/',
@@ -352,12 +532,20 @@ export default {
 				}
 			});
 			if (response && response.data && response.data.artisttracks && response.data.artisttracks.track) {
-				this.artistData = response.data.artisttracks.track;
+				return response.data.artisttracks.track;
 			}
 		},
 		getTimesPlayed(data) {
 			const reducer = (accumulator, currentvalue) => accumulator + Number(currentvalue.playcount);
 			return data.reduce(reducer, 0);
+		},
+		artistToCompare(index) {
+			const indexOf = this.compareArtists.indexOf(index);
+			if (indexOf === -1) {
+				this.compareArtists.push(index);
+			} else {
+				this.compareArtists.splice(indexOf, 1);
+			}
 		}
 	}
 };
@@ -451,9 +639,27 @@ svg {
 	path {
 		cursor: pointer;
 	}
-	// .middle-text text {
-	// 	font-size: rem(32);
-	// }
+}
+.compare {
+	position: fixed;
+	top: 0;
+	right: 0;
+	clip-path: inset(0 0 0 100%);
+	transition: 0.2s ease-in-out;
+	z-index: 2;
+	&--button {
+		background: color(White);
+		font-size: rem(16);
+		font-family: $secondary-font-family;
+		border: none;
+		box-shadow: none;
+		padding: rem(20 15);
+		cursor: pointer;
+		width: rem(200);
+	}
+	&.active {
+		clip-path: inset(0 0 0 0);
+	}
 }
 .svg_container {
 	position: sticky;
@@ -461,6 +667,13 @@ svg {
 	top: 0;
 	clip-path: inset(0 0 0 0);
 	width: 75vw;
+	&--compare {
+		position: sticky;
+		align-self: flex-start;
+		top: 0;
+		clip-path: inset(0 0 0 0);
+		width: 75vw;
+	}
 }
 .artists {
 	&__list {
@@ -506,6 +719,10 @@ svg {
 		&:hover,
 		&.hover {
 			background: darken(DarkBlue, 4%);
+			transition: 0.2s linear;
+		}
+		&.compared {
+			background: darken(DarkBlue, 10%);
 			transition: 0.2s linear;
 		}
 	}
